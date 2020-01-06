e9411414da4123ea9a36294dbca15f27e0aeea91
waynezdev <cas021919@coderacademy.edu.au>
2020-01-06 15:28:43 +1100

    added clock ticking interval

--- src/App.js
+++ src/App.js
@@ -1,16 +1,139 @@
-import React from 'react';
+import React, {Component, Suspense} from 'react';
 import Clock from "./Clock";
 
-const App = (props) => {
-    return (
+class App extends Component {
+
+   
+    state = {latitude: null,
+    error: null,
+    date: new Date()
+    };
+
+    componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+        (position) => {
+            this.setState({ latitude: position.coords.latitude});
+            
+        },
+        (error) => {
+
+            this.setState({error: "something went wrong"})
+        })
+
+    setInterval(() => {
+        this.setState({date : new Date()})
+    }, 1000)
+
+
+    }
+
+    
+
+    isItWarm =() => {
+        // const { latitude} = this.state;
+        // const month = new Date().getMonth() + 1;
+
+        // if (
+        // latitude < 0 && (month <3 || month > 9) || 
+        // latitude > 0 && (month >=3 || month <=9) || 
+        // latitude === 0) {
+
+        //     return true
+        // }
+
+        const { latitude } = this.state;
+
+        const month = new Date().getMonth();
+    
+        if (!latitude) {
+    
+          return false;
+    
+        }
+    
+        if (month >= 2 && month <= 8) {
+    
+          if (latitude < 0) {
+    
+            // southern hemisphere during winter
+    
+            return false;
+    
+          }
+    
+          // northern hemisphere during summer
+    
+          return true;
+    
+        } else {
+    
+          if (latitude < 0) {
+    
+            // southern hemisphere during summer
+    
+            return true;
+    
+          }
+    
+          // northern hemisphere during winter
+    
+          return false;
+    
+        }
+  
+    }
+
+
+    // weite a getClockIcon method that uses the isItWarm
+    // if true return sun.svg
+    // if false return snowflake.svg
+
+    getClockIcon = () => {
+
+        if (this.isItWarm()) {
+
+            return "sun.svg"
+
+        } else {
+            return "snowflake.svg"
+        }
+
+
+    }
+
+
+    render() {
+        const { latitude, error } = this.state;
+
+        // if (!latitude) {
+        //     return null;
+        // }
+        
+        if (error) {
+         return ( 
+         <div>
+                <p>{error}</p>
+
+            </div>
+        )
+        }
+
+        
+
+        return (
         <div>
-            <Clock 
-                icon="sun.svg"
-                timezone={"Sydney/Australia"} 
-                date={new Date()} 
+
+            {latitude}
+
+            <Clock
+                icon= {latitude !== null? this.getClockIcon() : null }
+                timezone={"Sydney/Australia"}
+                date={new Date()}
             />
         </div>
-    );
+
+        )
+    }
 }
 
 export default App;
