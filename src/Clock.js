cbe9922f03b767de819ebb21517f563f9e0642d3
waynezdev <cas021919@coderacademy.edu.au>
2020-01-06 15:44:04 +1100

    converted clock component from funct to class component

--- src/Clock.js
+++ src/Clock.js
@@ -1,29 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Clock.css";
 
-const Clock = (props) => {
-    const seconds = props.date.getSeconds();
-    const minutes = props.date.getMinutes();
-    const hours = props.date.getHours();
-    
-    return (
-        <div className="container">
-            <h3 className="label">{props.timezone}</h3>
-            <div className="clock-face" style={{ backgroundImage: `url(/${props.icon})` }}>
-                <div className="clock">
-                    <div className="hours-container">
-                        <div className="hours" style={{ transform: `rotateZ(${(hours * 30) + (minutes / 2)}deg)`}}></div>
-                    </div>
-                    <div className="minutes-container">
-                        <div className="minutes" style={{ transform: `rotateZ(${(minutes * 6)}deg)`}}></div>
-                    </div>
-                    <div className="seconds-container">
-                        <div className="seconds" style={{ transform: `rotateZ(${(seconds * 6)}deg)`}}></div>
+class Clock extends Component {
+
+    render() {
+        const seconds = this.props.date.getSeconds();
+        const minutes = this.props.date.getMinutes();
+        const hours = this.props.date.getHours();
+        
+        
+        return (
+            <div className="container">
+                <h3 className="label">{this.props.timezone}</h3>
+                <div className="clock-face" style={{ backgroundImage: `url(/${this.props.icon})` }}>
+                    <div className="clock">
+                        <div className="hours-container">
+                            <div className="hours" style={{ transform: `rotateZ(${(hours * 30) + (minutes / 2)}deg)`}}></div>
+                        </div>
+                        <div className="minutes-container">
+                            <div className="minutes" style={{ transform: `rotateZ(${(minutes * 6)}deg)`}}></div>
+                        </div>
+                        <div className="seconds-container">
+                            <div className="seconds" style={{ transform: `rotateZ(${(seconds * 6)}deg)`}}></div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+
+    }
 }
 
 export default Clock;
